4ae83adbf1fd9ade84777349f4b28460081755eb
Dominic Parducci <dominic.parducci@Dominics-MacBook-Pro.local>
2019-06-14 14:48:27 -0700

    authorization added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.urls import reverse
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
 # Create your models here. Don't forget to register them in admin.py (admin.site.register(modelname))
