2e88923e16bf9e4ee0b043a67b63da3c4431e240
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-06-14 15:47:53 -0700

    set up inheritance template and about page

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
