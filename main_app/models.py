430cf95ce0b834910d09bfeb2c1433a5b02f3615
Calvin Féau <calvin@Calvins-MacBook-Pro.local>
2019-06-14 17:00:55 -0700

    create Match model + register to admin

--- main_app/models.py
+++ main_app/models.py
@@ -3,4 +3,34 @@ from django.urls import reverse
 from datetime import date
 # Import the User
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here. Don't forget to register them in admin.py (admin.site.register(modelname))
+
+class Match(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = PhoneNumberField()
+    age = models.IntegerField()
+    location = models.TextField()
+    meet = models.TextField()
+    interests = models.TextField()
+    attraction = models.IntegerField(
+        choices=(
+            (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)
+        )
+    )
+    zodiac = models.CharField(
+        max_length=100,
+        choices=(
+            ('Aries', 'Aries'), ('Taurus', 'Taurus'), ('Gemini', 'Gemini'),
+            ('Cancer', 'Cancer'), ('Leo', 'Leo'), ('Virgo', 'Virgo'), ('Libra', 'Libra'),
+            ('Scorpio', 'Scorpio'), ('Sagittarius', 'Sagittarius'), ('Capricorn', 'Capricorn'),
+            ('Aquarius', 'Aquarius'), ('Pisces','Pisces')
+        ),
+    )
+    see_again = models.BooleanField()
+    ghost = models.BooleanField()
+
+    def __str__(self):
+        return self.name
