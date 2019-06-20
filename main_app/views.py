67df48cf04d0f1518beb9b794f3e0d59c1b502b1
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-06-19 17:11:20 -0700

    Add Google Cal API

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django import forms
@@ -13,6 +14,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
+# Cal API imports
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from django.utils import timezone
+from tz_detect.templatetags.tz_detect import tz_detect
 
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'datebase'
@@ -180,3 +190,55 @@ class ProfileCreate (LoginRequiredMixin, CreateView):
 class ProfileUpdate(LoginRequiredMixin, UpdateView):
   model = Profile
   fields = ['first_name', 'last_name', 'age', 'gender', 'zodiac', 'apps_used', 'relationship_goal']
+
+
+
+# Google Calendar API
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def cal(request, pk):
+  creds = None
+  # The file token.pickle stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists('token.pickle'):
+      with open('token.pickle', 'rb') as token:
+          creds = pickle.load(token)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+          creds.refresh(Request())
+      else:
+          flow = InstalledAppFlow.from_client_secrets_file(
+              'credentials.json', SCOPES)
+          creds = flow.run_local_server()
+      # Save the credentials for the next run
+      with open('token.pickle', 'wb') as token:
+          pickle.dump(creds, token)
+
+  service = build('calendar', 'v3', credentials=creds)
+
+  # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+  # Environment setup above, add event below
+  
+  rdv = Rdv.objects.get(pk=pk)
+  event = {
+      'summary': f'Meet with {rdv.match.name}',
+      'location': f'{rdv.where}',
+      'description': f'{rdv.what}',
+      'start': {
+          'dateTime': f'{rdv.date}T{rdv.rdv_time}-07:00',
+          'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+          'dateTime': f'{rdv.date}T{rdv.rdv_time}-07:00',
+          'timeZone': 'America/Los_Angeles',
+      }
+      }
+
+  event = service.events().insert(calendarId='primary', body=event).execute()
+  return redirect('rdv_detail', pk=pk)
+  # print ('Event created: %s' % (event.get('htmlLink')))
+
