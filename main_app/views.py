6ee0d5973ad35c8ea317a6a792ad8679bace8db6
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-06-20 13:38:49 -0700

    aws boto change

--- main_app/views.py
+++ main_app/views.py
@@ -46,7 +46,7 @@ def add_match_photo(request, match_id):
   print(match_id)
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
-    s3 = session.client('s3')
+    s3 = boto3.client('s3')
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
     try:
       s3.upload_fileobj(photo_file, BUCKET, key)
@@ -61,7 +61,7 @@ def add_match_photo(request, match_id):
 def add_profile_photo(request, profile_id):
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
-    s3 = session.client('s3')
+    s3 = boto3.client('s3')
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
     try:
       s3.upload_fileobj(photo_file, BUCKET, key)
